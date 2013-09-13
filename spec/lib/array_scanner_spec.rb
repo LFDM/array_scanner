d754894c523ce55157bf0814534fb9d4f563ac24
LFDM <1986gh@gmail.com>
2013-09-13 23:37:58 +0200

    Dumps should terminology in rspec examples.

--- spec/lib/array_scanner_spec.rb
+++ spec/lib/array_scanner_spec.rb
@@ -3,7 +3,7 @@ require 'array_scanner'
 
 describe ArrayScanner do
   describe "#initialize" do
-    it "should raise type errors for wrong arguments" do
+    it "raises type errors for wrong arguments" do
       expect { ArrayScanner.new({}) }     .to raise_error(TypeError)
       expect { ArrayScanner.new([], nil) }.to raise_error(TypeError)
     end
@@ -12,7 +12,7 @@ describe ArrayScanner do
   let(:as) { ArrayScanner.new([1,2,3,4,5]) }
 
   describe "#size" do
-    it "should return the arrays size" do
+    it "returns the arrays size" do
       as.size.should be 5
     end
   end
@@ -24,24 +24,24 @@ describe ArrayScanner do
   end
 
   describe "#eoa" do
-    it "should return last valid position, i.d. end of array" do
+    it "returns last valid position, i.d. end of array" do
       as.eoa.should be 4
     end
   end
 
   describe "#eoa?" do
-    it "should return false if not at end of array" do
+    it "returns false if not at end of array" do
       as.eoa?.should be_false
     end
 
-    it "should return true if at end of array" do
+    it "returns true if at end of array" do
       as.position = 4
       as.eoa?.should be_true
     end
   end
 
   describe "#current_element" do
-    it "should return element at the pointers position" do
+    it "returns element at the pointers position" do
       as.position = 2
       as.current_element.should be 3
     end
@@ -62,7 +62,7 @@ describe ArrayScanner do
   end
 
   describe "#position" do
-    it "should return current position" do
+    it "returns current position" do
       as.position.should be 0
     end
   end
@@ -80,21 +80,21 @@ describe ArrayScanner do
   end
 
   describe "#position=" do
-    it "should set new position" do
+    it "sets new position" do
       as.position = 4
       as.position.should be 4
     end
 
-    it "should raise error when new position is invalid type" do
+    it "raises error when new position is invalid type" do
       expect { as.position = "" }.to raise_error(TypeError)
     end
 
-    it "should raise error when new position is outside of arrays range" do
+    it "raises error when new position is outside of arrays range" do
       expect { as.position = 6 }.to raise_error(ArgumentError)
       expect { as.position = 5 }.to raise_error(ArgumentError)
     end
 
-    it "should add to positions history stack" do
+    it "adds to positions history stack" do
       [1,2,3].each { |n| as.position = n }
       as.pos_hist.stack.should == [2,1,0]
     end
@@ -115,12 +115,12 @@ describe ArrayScanner do
   end
 
   describe "#surroundings" do
-    it "should return element in front and element behind pointer" do
+    it "returns element in front and element behind pointer" do
       as.pos = 2
       as.surroundings.should == [2,4]
     end
 
-    it "should return nil elements for edge positions" do
+    it "returns nil elements for edge positions" do
       as.surroundings.should == [nil, 2]
       as.terminate
       as.surroundings.should == [4, nil]
@@ -128,19 +128,19 @@ describe ArrayScanner do
   end
 
   describe "#last_position" do
-    it "should return last position after recent movement" do
+    it "returns last position after recent movement" do
       as.position = 1 and as.position = 2
       as.last_position.should be 1
     end
   end
 
   describe "#last_positions" do
-    it "should return array of all pointer movements when called without arg" do
+    it "returns array of all pointer movements when called without arg" do
       as.position = 3 and as.position = 1 and as.position = 4
       as.last_positions.should == [1,3,0]
     end
 
-    it "should return array of all pointer movements according to argument number" do
+    it "returns array of all pointer movements according to argument number" do
       as.position = 3 and as.position = 1 and as.forward(1)
       as.last_positions(2).should == [1,3]
     end
@@ -152,20 +152,20 @@ describe ArrayScanner do
     end
 
     context "without argument" do
-      it "should reset pointer to last position and return new value" do
+      it "resets pointer to last position and return new value" do
         as.unscan.should be 4
         as.position.should be 4
       end
     end
 
     context "with argument" do
-      it "should reset pointer by n steps and return new value" do
+      it "resets pointer by n steps and return new value" do
         as.unscan(3).should be 2
         as.position.should be 2
       end
     end
 
-    it "should return nil when position history is empty" do
+    it "returns nil when position history is empty" do
       as.pos_hist.stack.clear
       as.unscan.should be_nil
     end
@@ -173,12 +173,12 @@ describe ArrayScanner do
   end
 
   describe "#last_result" do
-    it "should return last result" do
+    it "returns last result" do
       as.scan
       as.last_result.should == 1
     end
 
-    it "should return last valid result with true argument" do
+    it "returns last valid result with true argument" do
       as.scan and as.scan { |x| x == 100 }
       as.last_result.should be_false
       as.last_result(true).should == 1
@@ -199,24 +199,24 @@ describe ArrayScanner do
   end
 
   describe "#forward" do
-    it "should forward pointer by argument" do
+    it "forwards pointer by argument" do
       as.forward(2)
       as.position.should be 2
     end
 
-    it "should foward pointer to eoa if new position is outside of range" do
+    it "fowards pointer to eoa if new position is outside of range" do
       as.forward(10)
       as.position.should be 4
     end
   end
 
   describe "#forward_to" do
-    it "should forward pointer and return new position if block is true" do
+    it "forwards pointer and return new position if block is true" do
       as.forward_to { |el| el == 3 }.should be 2
       as.position.should be 2
     end
 
-    it "should not forward pointer and return nil if block is false" do
+    it "does not forward pointer and return nil if block is false" do
       as.forward_to { |el| el == 7 }.should be_nil
       as.position.should be 0
     end
@@ -227,13 +227,13 @@ describe ArrayScanner do
   end
 
   describe "#rewind" do
-    it "should rewind pointer by argument" do
+    it "rewinds pointer by argument" do
       as.position = 3
       as.rewind(2)
       as.position.should be 1
     end
 
-    it "should rewind to zero if new position is outside of range" do
+    it "rewinds to zero if new position is outside of range" do
       as.position = 2
       as.rewind(4)
       as.position.should be 0
@@ -241,13 +241,13 @@ describe ArrayScanner do
   end
 
   describe "#rewind_to" do
-    it "should rewind pointer to new position if block is true" do
+    it "rewinds pointer to new position if block is true" do
       as.position = 4
       as.rewind_to { |el| el == 2 }.should be 1
       as.position.should be 1
     end
 
-    it "should not rewind pointer and return nil if block is false" do
+    it "does not rewind pointer and return nil if block is false" do
       as.position = 4
       as.rewind_to { |el| el == 5 }.should be_nil
       as.position.should be 4
@@ -259,7 +259,7 @@ describe ArrayScanner do
   end
 
   describe "#reset" do
-    it "should reset pointer to 0" do
+    it "resets pointer to 0" do
       as.position = 4
       as.reset
       as.position.should be 0
@@ -267,7 +267,7 @@ describe ArrayScanner do
   end
 
   describe "#terminate" do
-    it "should set pointer to eoa position" do
+    it "sets pointer to eoa position" do
       as.terminate
       as.position.should be 4
     end
@@ -280,28 +280,28 @@ describe ArrayScanner do
   end
 
   describe "#scanned" do
-    it "should return already scanned elements" do
+    it "returns already scanned elements" do
       as.position = 3
       as.scanned.should == [1,2,3]
     end
   end
 
   describe "#scanned_size" do
-    it "should return number of scanned elements" do
+    it "returns number of scanned elements" do
       as.position = 3
       as.scanned_size.should be 3
     end
   end
 
   describe "#rest" do
-    it "should return remaining elements" do
+    it "returns remaining elements" do
       as.position = 1
       as.rest.should == [2,3,4,5]
     end
   end
 
   describe "#rest_size" do
-    it "should return number of remaining elements" do
+    it "returns number of remaining elements" do
       as.position = 1
       as.rest_size.should be 4
     end
@@ -309,18 +309,18 @@ describe ArrayScanner do
 
   describe "#scan" do
     context "without blk" do
-      it "should return array element at current position and advance pointer" do
+      it "returns array element at current position and advance pointer" do
         as.position = 2
         as.scan.should be 3
         as.position.should be 3
       end
 
-      it "should not advance pointer with false argument" do
+      it "does not advance pointer with false argument" do
         as.scan(false)
         as.position.should be 0
       end
 
-      it "should not advance pointer if at eoa" do
+      it "does not advance pointer if at eoa" do
         as.position = 4
         as.scan
         as.position.should be 4
@@ -328,17 +328,17 @@ describe ArrayScanner do
     end
 
     context "with blk" do
-      it "should return false for false block" do
+      it "returns false for false block" do
         as.scan { |el| el == 6 }.should be_false
       end
 
-      it "should return array element at current position for true block and advance position by 1" do
+      it "returns array element at current position for true block and advance position by 1" do
         as.position = 2
         as.scan { |el| el == 3 }.should be 3
         as.position.should be 3
       end
 
-      it "should not advance pointer with false argument" do
+      it "does not advance pointer with false argument" do
         as.scan(false) { |el| el == 3 }.should be false
         as.position.should be 0
       end
@@ -346,22 +346,22 @@ describe ArrayScanner do
   end
 
   describe "#scan_until" do
-    it "should return array from current position to position before the block evaluated to true and move pointer" do
+    it "returns array from current position to position before the block evaluated to true and move pointer" do
       as.scan_until { |el| el == 3 }.should == [1,2]
       as.position.should be 2
     end
 
-    it "should return false when block is false and not move pointer." do
+    it "returns false when block is false and not move pointer." do
       as.scan_until { |el| el == 8 }.should be_false
       as.position.should be 0
     end
 
-    it "should include the element for which the block evaluated true when true argument is given." do
+    it "includes the element for which the block evaluated true when true argument is given." do
       as.scan_until(true) { |el| el == 3 }.should == [1,2,3]
       as.position.should be 3
     end
 
-    it "should rest pointer at eoa when true argument is given and the truthy result is at eoa." do
+    it "rests pointer at eoa when true argument is given and the truthy result is at eoa." do
       as.scan_until(true) { |el| el == 5 }.should == [1,2,3,4,5]
       as.position.should == as.eoa
     end
@@ -372,24 +372,24 @@ describe ArrayScanner do
   end
 
   describe "#peek" do
-    it "should return next element without argument" do
+    it "returns next element without argument" do
       as.position = 1
       as.peek.should == 2
     end
 
-    it "should array of next elements by argument number" do
+    it "arrays of next elements by argument number" do
       as.peek(1).should == [1]
       as.peek(2).should == [1,2]
     end
   end
 
   describe "#peek_until" do
-    it "should return array of next elements between current position and the element for which the block is true" do
+    it "returns array of next elements between current position and the element for which the block is true" do
       as.position = 1
       as.peek_until { |el| el == 5 }.should == [2,3,4]
     end
 
-    it "should return empty array if block is false" do
+    it "returns empty array if block is false" do
       as.peek_until { |el| el == 6 }.should == []
     end
 
@@ -399,38 +399,38 @@ describe ArrayScanner do
   end
 
   describe "#look_behind" do
-    it "should return element behind pointer (i.e. pointer position - 1), when no arguemnt is given" do
+    it "returns element behind pointer (i.e. pointer position - 1), when no arguemnt is given" do
       as.pos = 2
       as.look_behind.should be 2
     end
 
-    it "should array of previous elements by argument number (reverse order)" do
+    it "arrays of previous elements by argument number (reverse order)" do
       as.pos = 4
       as.look_behind(1).should == [4]
       as.look_behind(2).should == [4,3]
     end
 
-    it "should return nil when there is nothing behind without argument" do
+    it "returns nil when there is nothing behind without argument" do
       as.look_behind.should be_nil
     end
 
-    it "should return empty array when there is nothing behind with argument" do
+    it "returns empty array when there is nothing behind with argument" do
       as.look_behind(3).should == []
     end
 
-    it "should return everything behind when argument number is larger than number of elements behind" do
+    it "returns everything behind when argument number is larger than number of elements behind" do
       as.pos = 2
       as.look_behind(4).should == [2,1]
     end
   end
 
   describe "#look_behind_until" do
-    it "should return arguemnt of previous element between current position and the element for which the block is true - in reverse order" do
+    it "returns arguemnt of previous element between current position and the element for which the block is true - in reverse order" do
       as.pos = 4
       as.look_behind_until { |el| el == 2 }.should == [4,3]
     end
 
-    it "should return empty array if block is false" do
+    it "returns empty array if block is false" do
       as.look_behind_until { |el| el == 1 }.should == []
     end
 
@@ -440,12 +440,12 @@ describe ArrayScanner do
   end
 
   describe "#find" do
-    it "should return next element after pointer for true block" do
+    it "returns next element after pointer for true block" do
       as.position = 1
       as.find { |x| x == 3 }.should be 3
     end
 
-    it "should return nil for no find - e.g. pointer is already behind the looked up element" do
+    it "returns nil for no find - e.g. pointer is already behind the looked up element" do
       as.position = 3
       as.find { |x| x == 0 }.should be_nil
     end
@@ -463,7 +463,7 @@ describe ArrayScanner do
   end
 
   describe "#previous" do
-    it "should return previous element in front of pointer for true block" do
+    it "returns previous element in front of pointer for true block" do
       as.position = 3
       as.previous { |x| x == 2 }.should be 2
     end
@@ -474,26 +474,26 @@ describe ArrayScanner do
   end
 
   describe "#rr" do
-    it "should add to results history and return the argument" do
+    it "adds to results history and return the argument" do
       as.rr(1).should be 1
       as.res_hist.stack.should == [1]
     end
   end
 
   describe "#to_a" do
-    it "should return simple array" do
+    it "returns simple array" do
       as.to_a.should == [1,2,3,4,5]
     end
   end
 
   describe "#to_s" do
-    it "should return simple array to_s" do
+    it "returns simple array to_s" do
       as.to_s.should == "[1, 2, 3, 4, 5]"
     end
   end
 
   describe "#inspect" do
-    it "should return pointer information" do
+    it "returns pointer information" do
       as.pos = 2
       as.inspect.should == "Pointer at 2/4. Current element: 3"
     end
--- spec/lib/hist_spec.rb
+++ spec/lib/hist_spec.rb
@@ -3,11 +3,11 @@ require 'hist'
 
 describe Hist do
   describe "#initialize" do
-    it "should come with default stack size 10" do
+    it "comes with default stack size 10" do
       Hist.new.max_size.should be 10
     end
 
-    it "should take max sizes as argument" do
+    it "takes max size as argument" do
       Hist.new(11).max_size.should be 11
     end
   end
@@ -24,12 +24,12 @@ describe Hist do
   describe "#push" do
     let(:hist) { Hist.new(3) }
 
-    it "should push new element to stack" do
+    it "pushes new element to stack" do
       hist.push(1)
       hist.stack.should == [1]
     end
 
-    it "should not take more elements than max size" do
+    it "does not take more elements than max size" do
       hist.instance_variable_set(:@stack, [1,2,3])
       hist.push(4)
       hist.stack.should == [4,1,2]
@@ -43,11 +43,11 @@ describe Hist do
       h
     end
 
-    it "should return recently added element to stack without argument" do
+    it "returns recently added element to stack without argument" do
       hist.recent.should be 1
     end
 
-    it "should return array of recently added elements according to argument number" do
+    it "returns array of recently added elements according to argument number" do
       hist.recent(2).should == [1,2]
     end
   end
